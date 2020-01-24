6e1ad99f96bb456a177c5389284a4099d9487d48
yat8823jp <yat@yat-net.com>
2020-01-24 14:16:57 +0900

    change-name

--- main.php
+++ main.php
@@ -1,94 +1,94 @@
 <?php
 /**
- * @package add-palecolor
+ * @package red-add-palecolor
  * @version 1.0.0
  */
 /*
-Plugin Name: Add Palecolor
-Plugin URI: 
+Plugin Name: RD Add Palecolor
+Plugin URI:
 Description: This plugin is add pale color palette
 Author: YAT
 Version: 1.0.0
 Author URI: http://wp.yat-net.com
 */
 
-function add_pale_color_pallet() {
+function rd_add_pale_color_pallet() {
 	add_theme_support( 'editor-color-palette', array(
 		array(
-			'name'  => __( 'ピンク', 'genesis-sample' ),
+			'name'  => __( 'Pale pink', 'add-palecolor' ),
 			'slug'  => 'pink',
 			'color'  => '#f78da7',
 		),
 		array(
-			'name'  => __( 'レッド', 'genesis-sample' ),
+			'name'  => __( 'Vivid red', 'add-palecolor' ),
 			'slug'  => 'red',
 			'color' => '#cf2e2e',
 		),
 		array(
-			'name'  => __( 'オレンジ', 'genesis-sample' ),
+			'name'  => __( 'Luminous vivid orange', 'add-palecolor' ),
 			'slug'  => 'orange',
 			'color' => '#ff6900',
 				 ),
 		array(
-			'name'  => __( '琥珀', 'genesis-sample' ),
+			'name'  => __( 'Light green cyan', 'add-palecolor' ),
 			'slug'  => 'amber',
 			'color' => '#fcb900',
 				 ),
 		array(
-			'name'  => __( '薄いグリーンシアン', 'genesis-sample' ),
+			'name'  => __( 'Light green cyan', 'add-palecolor' ),
 			'slug'  => 'light-green-cyan',
 			'color' => '#7bdcb5',
 				 ),
 		array(
-			'name'  => __( '鮮やかなグリーンシアン', 'genesis-sample' ),
+			'name'  => __( 'Vivid green cyan', 'add-palecolor' ),
 			'slug'  => 'vivid-green-cyan',
 			'color' => '#00d084',
 				 ),
 		array(
-			'name'  => __( '淡いシアンブルー', 'genesis-sample' ),
+			'name'  => __( 'Pale cyan blue', 'add-palecolor' ),
 			'slug'  => 'pale-cyan-blue',
 			'color' => '#8ed1fc',
 				 ),
 		array(
-			'name'  => __( '鮮やかなシアンブルー', 'genesis-sample' ),
+			'name'  => __( 'Vivid cyan blue', 'add-palecolor' ),
 			'slug'  => 'vivid-cyan-blue',
 			'color' => '#0693e3',
 				 ),
 		array(
-			'name'  => __( 'ライト・グレー', 'genesis-sample' ),
+			'name'  => __( 'Vivid purple', 'add-palecolor' ),
 			'slug'  => 'light-gray',
 			'color' => '#eeeeee',
 				 ),
 		array(
-			'name'  => __( 'シアンブルーグレー', 'genesis-sample' ),
+			'name'  => __( 'Very light gray', 'add-palecolor' ),
 			'slug'  => 'cyan-blue-gray',
 			'color' => '#abb8c3',
 				 ),
 		array(
-			'name'  => __( '濃灰', 'genesis-sample' ),
+			'name'  => __( 'Cyan bluish gray', 'add-palecolor' ),
 			'slug'  => 'dark-gray',
 			'color' => '#313131',
 				 ),
 		 array(
- 			'name'  => __( 'ペールオレンジ', 'themeLangDomain' ),
- 			'slug'  => 'orange',
+ 			'name'  => __( 'Pale orange', 'add-palecolor' ),
+ 			'slug'  => 'pale-orange',
  			'color' => '#FFF2D8',
  		),
 		array(
-			'name'  => __( 'ペールグリーン', 'themeLangDomain' ),
-			'slug'  => 'orange',
+			'name'  => __( 'Pale green', 'add-palecolor' ),
+			'slug'  => 'pale-green',
 			'color' => '#E5FCC2',
 		),
 		array(
-			'name'  => __( 'ペールブルー', 'themeLangDomain' ),
-			'slug'  => 'orange',
+			'name'  => __( 'Pale blue', 'add-palecolor' ),
+			'slug'  => 'pale-glue',
 			'color' => '#D5FFFC',
 		),
 		array(
-			'name'  => __( 'ペールピンク', 'themeLangDomain' ),
-			'slug'  => 'orange',
+			'name'  => __( 'Pale light pink', 'add-palecolor' ),
+			'slug'  => 'pale-light-pink',
 			'color' => '#FFEBF7',
 		),
 	) );
 }
-add_action( 'after_setup_theme', 'add_pale_color_pallet' );
+add_action( 'after_setup_theme', 'rd_add_pale_color_pallet' );
